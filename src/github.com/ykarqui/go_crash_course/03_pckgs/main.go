0898df9768e609775465b1d7e2641b51ff481243
Yamil Karqui <yamilkarqui@MAC-66320.local>
2022-10-27 20:28:55 -0300

    fix: adding remove package 3

--- src/github.com/ykarqui/go_crash_course/03_pckgs/main.go
+++ src/github.com/ykarqui/go_crash_course/03_pckgs/main.go
@@ -3,13 +3,11 @@ package main
 import (
 	"fmt"
 	"math"
-
-	"github.com/ykarqui/golang-practice/go-refresh/strutil"
+	// "github.com/ykarqui/golang-practice/go-refresh/strutil"
 )
 
 func main() {
 	fmt.Println(math.Floor(2.7))
 	fmt.Println(math.Ceil(2.7))
 	fmt.Println(math.Sqrt(2.7))
-	fmt.Println(strutil.Reverse("olleH"))
 }
