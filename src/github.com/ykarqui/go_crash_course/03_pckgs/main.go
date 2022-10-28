01260b8c23e60cb4b412cea7fe43dd1cfe91950d
Yamil Karqui <yamilkarqui@MAC-66320.local>
2022-10-28 12:46:39 -0300

    fix: go mod and package

--- src/github.com/ykarqui/go_crash_course/03_pckgs/main.go
+++ src/github.com/ykarqui/go_crash_course/03_pckgs/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"go-refresh/src/github.com/ykarqui/go_crash_course/03_pckgs/strutil"
 	"math"
 )
 
@@ -9,4 +10,5 @@ func main() {
 	fmt.Println(math.Floor(2.7))
 	fmt.Println(math.Ceil(2.7))
 	fmt.Println(math.Sqrt(2.7))
+	fmt.Println(strutil.Reverse("topaet"))
 }
