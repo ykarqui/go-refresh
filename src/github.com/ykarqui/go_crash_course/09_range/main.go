aee54533c5f52cb5581a529ddc3b343cfa2fa953
Yamil Karqui <yamilkarqui@MAC-66320.local>
2022-10-29 01:03:00 -0300

    feat: add range

--- src/github.com/ykarqui/go_crash_course/09_range/main.go
+++ src/github.com/ykarqui/go_crash_course/09_range/main.go
@@ -1,5 +1,32 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	ids := []int{33, 76, 54, 23, 11, 2}
+
+	// Loop through ids
+	for i, id := range ids {
+		fmt.Printf("%d - ID: %d\n", i, id)
+	}
+
+	// Not using index
+	for _, id := range ids {
+		fmt.Printf("ID: %d\n", id)
+	}
+
+	// Add ids together
+	sum := 0
+	for _, id := range ids {
+		sum += id
+	}
+	fmt.Println("Sum ", sum)
+
+	// Range with map
+	emails := map[string]string{"Bob": "bob@gmail.com", "Carl": "cc@gmail.com", "Coli": "js@gmail.com"}
+
+	for k, v := range emails {
+		fmt.Printf("%s: %s \n", k, v)
+	}
 }
