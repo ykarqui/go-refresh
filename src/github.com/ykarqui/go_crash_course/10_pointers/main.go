6e0e0d196c804671105d314634db3b71c10942e5
Yamil Karqui <yamilkarqui@MAC-66320.local>
2022-10-29 01:09:54 -0300

    feat: add pointers

---
+++ src/github.com/ykarqui/go_crash_course/10_pointers/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 5
+	b := &a
+
+	fmt.Println(a, b)
+	fmt.Printf("%T\n", b)
+
+	// user * to real val from address
+	fmt.Println(*b)
+
+	// change val with pointer
+	*b = 10
+	fmt.Println(a, *b)
+}
